447d8d5d7aeb2acd5a8b3fc858867d9a3017b93c
Ajay Kotian <ajay.kotian@csusb.edu>
2019-02-01 16:34:10 -0800

    Initialized React component

---
+++ src/public/js/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ClassSearchContainer } from './ClassSearchContainer';
+
+ReactDOM.render(<ClassSearchContainer />, document.querySelector('#class-search-form-wrapper'));
