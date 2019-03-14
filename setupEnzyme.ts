f80cbaa89063d0e69dc3becb169f2202b5e3da9d
Ajay Kotian <ajay.kotian@csusb.edu>
2019-03-13 17:50:37 -0700

    test: added global variables for localStorage and currentQuarterId

--- setupEnzyme.ts
+++ setupEnzyme.ts
@@ -5,3 +5,15 @@ import 'jsdom-global/register';
 Enzyme.configure({
     adapter: new Adapter(),
 });
+
+global.requestAnimationFrame = (callback) => {
+    setTimeout(callback, 0);
+};
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
+global.currentQuarterId = '2194';
