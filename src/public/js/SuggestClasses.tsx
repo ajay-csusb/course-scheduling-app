76be6324fb65774b40089587819fdfde48674e5e
Ajay Kotian <ajay.kotian@csusb.edu>
2019-03-13 18:34:15 -0700

    feat: added autocomplete functionality to search subjects

--- src/public/js/SuggestClasses.tsx
+++ src/public/js/SuggestClasses.tsx
@@ -1,29 +1,28 @@
 import { IItemRendererProps } from '@blueprintjs/select';
-import { IClass } from './Class';
 import React from 'react';
 import { MenuItem } from '@blueprintjs/core';
+import { ISubject } from './Subject';
 
-export function filterClasses(query: any, classes: any): boolean {
-  return `${classes.name.toLowerCase()} ${classes.classNumber}`.indexOf(query.toLowerCase()) >= 0;
+export function filterClasses(query: any, subject: any): boolean {
+  return `${subject.name}`.toLowerCase().indexOf(query.toLowerCase()) >= 0;
 }
 
-export function renderMenuItem(classes: IClass, classRenderer: IItemRendererProps): JSX.Element {
+export function renderMenuItem(subject: ISubject, classRenderer: IItemRendererProps): JSX.Element {
   return (
     <MenuItem
       active={false}
       disabled={false}
-      label={classes.name}
-      key={classes.courseId}
+      label={subject.abbr}
+      key={subject.abbr}
       onClick={classRenderer.handleClick}
-      text={`${classes.subject} ${classes.classNumber}`}
+      text={subject.name}
       className="search-autocomplete-items"
     />
-
   );
 }
 
-export function renderInputValue(classes: IClass): string {
-  return classes.name;
+export function renderInputValue(subject: ISubject): string {
+  return `${subject.name} (${subject.abbr})`;
 }
 
 export const renderClassSuggestProps = {
