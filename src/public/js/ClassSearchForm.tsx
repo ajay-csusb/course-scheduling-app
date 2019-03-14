f0305009f1b4dfdcc3e95f7393b00f956bffb7d8
Ajay Kotian <ajay.kotian@csusb.edu>
2019-03-13 18:04:14 -0700

    feat: added fields to search form

--- src/public/js/ClassSearchForm.tsx
+++ src/public/js/ClassSearchForm.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react';
 import { FormGroup, Button, ControlGroup } from '@blueprintjs/core';
-import { IMeetingTime, IMeetingDate } from './ClassSearchContainer';
 import { Suggest } from '@blueprintjs/select';
 import { renderClassSuggestProps } from './SuggestClasses';
 import { RadioGroupQuarter } from './RadioGroupQuarter';
 import { RadioGroupCampus } from './RadioGroupCampus';
 import { ControlGroupMeetingTime } from './ControlGroupMeetingTime';
 import { ControlGroupMeetingDate } from './ControlGroupMeetingDate';
+import { IClass, IMeetingTime, IMeetingDate } from './Class';
+import { RadioGroupInstructionMode } from './RadioGroupInstructionMode';
+import { renderInstructorSuggestProps } from './SuggestInstructors';
+import { ISubject } from './Subject';
 
 interface ClassSearchFormProps {
-  classes: [];
+  classes: IClass[];
   quarter: string;
   campus: string;
-  courseId: string;
+  subjects: ISubject[];
   meetingTime: IMeetingTime;
   meetingDate: IMeetingDate;
+  instructionMode: string;
+  instructorName: string;
+  instructors: string[];
   isReset?: boolean;
   onChangeOfQuarter: (event: React.FormEvent) => void;
   onChangeOfCampus: (event: React.FormEvent) => void;
   onChangeOfMeetingTime: (event: any) => void;
   onChangeOfMeetingDate: (event: any) => void;
-  onChangeOfClassName: (event: any) => void;
+  onChangeOfSubject: (event: any) => void;
+  onChangeOfInstructionMode: (event: any) => void;
+  onChangeOfInstructor: (event: any) => void;
+  onSubmit: (event: any) => void;
+  onReset: (event: any) => void;
 }
 
 export class ClassSearchForm extends React.Component<ClassSearchFormProps, {}> {
@@ -46,15 +56,26 @@ export class ClassSearchForm extends React.Component<ClassSearchFormProps, {}> {
           onChangeOfQuarter={this.props.onChangeOfQuarter}
         />
         <ControlGroup>
-          <label>Enter class:</label>
+          <label>Select a Subject:</label>
           <SuggestClasses
             {...renderClassSuggestProps}
-            items={this.props.classes}
+            items={this.getSubjects()}
             className="search-autocomplete"
-            onItemSelect={this.props.onChangeOfClassName}
+            openOnKeyDown={false}
+            onItemSelect={this.props.onChangeOfSubject}
           />
         </ControlGroup>
         <br/>
+        <ControlGroup>
+          <label>Instructor:</label>
+          <SuggestClasses
+            {...renderInstructorSuggestProps}
+            items={this.getInstructors()}
+            className="search-instructor-autocomplete"
+            openOnKeyDown={false}
+            onItemSelect={this.props.onChangeOfInstructor}
+          />
+        </ControlGroup>
         <RadioGroupCampus
           campus={this.props.campus}
           onChangeOfCampus={this.props.onChangeOfCampus}
@@ -67,9 +88,21 @@ export class ClassSearchForm extends React.Component<ClassSearchFormProps, {}> {
           meetingDate={this.props.meetingDate}
           onChangeOfMeetingDate={this.props.onChangeOfMeetingDate}
         />
-        <Button text="Submit" type="submit" className="bp3-intent-primary" />
-        <Button text="Reset" style={{ float: 'right' }} type="reset" />
+        <RadioGroupInstructionMode
+          instructionMode={this.props.instructionMode}
+          onChangeOfInstructionMode={this.props.onChangeOfInstructionMode}
+        />
+        <Button text="Submit" onClick={this.props.onSubmit} type="submit" className="bp3-intent-primary" />
+        <Button text="Reset" onClick={this.props.onReset} style={{ float: 'right' }} type="reset" />
       </FormGroup >
     );
+  }
+
+  private getSubjects(): ISubject[] {
+    return this.props.subjects;
+  }
+
+  private getInstructors(): string[] {
+    return this.props.instructors;
   }
 }
