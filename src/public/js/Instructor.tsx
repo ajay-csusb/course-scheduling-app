8101825556986e1d1642b09e0802aca7d574174b
Ajay Kotian <ajay.kotian@csusb.edu>
2019-03-13 18:28:46 -0700

    feat: added a class to represent an Instructor

---
+++ src/public/js/Instructor.tsx
@@ -0,0 +1,25 @@
+import * as ClassSearchUtils from './ClassSearchUtils';
+import { UserInput } from './UserInput';
+import { IClass } from './Class';
+export class Instructor {
+
+  static instructorUrl = 'http://webdx.csusb.edu/ClassSchedule/getAllInstuctor ';
+  public static getAllInstructors(onSuccess: (response: any) => void,
+                                  onFailure: (error: string) => void, url: string = this.instructorUrl): void {
+    ClassSearchUtils.fetchData(url, onSuccess, onFailure);
+  }
+
+  public static filter(classes: IClass[] , uInput: UserInput): IClass[] {
+    const result: IClass[] = [];
+    classes.forEach((_class: IClass) => {
+      if (uInput.getInstructor() === '') {
+        result.push(_class);
+      }
+      if (_class.instructorName.toLowerCase() === uInput.getInstructor().toLowerCase()) {
+        result.push(_class);
+      }
+    });
+    return result;
+  }
+
+}
