777cae02b1312d7380e31a4fce1efb177f0706a3
Ajay Kotian <ajay.kotian@csusb.edu>
2019-03-13 18:09:50 -0700

    feat: added table row to display classes

--- src/public/js/ClassesRow.tsx
+++ src/public/js/ClassesRow.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { IClass, Class } from './Class';
 
 interface ClassesRowProps {
-  classes?: any;
+  classes: IClass;
   quarter?: string;
   campus?: string;
   className?: string;
@@ -10,23 +11,25 @@ interface ClassesRowProps {
 }
 export class ClassesRow extends React.Component<ClassesRowProps> {
   public render(): JSX.Element {
-    const classDetails = this.props.classes;
+    const classDetails: IClass = this.props.classes;
+    const classObj = new Class(classDetails);
+    const days = classObj.getClassMeetingDates();
     return (
       <tr>
-        <td>{`${classDetails.subject} ${classDetails.catalog_NBR} ${classDetails.class_SECTION}`}</td>
-        <td>{classDetails.crse_ID}</td>
-        <td>{classDetails.session_CODE}</td>
-        <td>{classDetails.descr}</td>
-        <td>{classDetails.instruction_MODE}</td>
-        <td>{classDetails.csu_APDB_CMP_UNITS}</td>
-        <td>Days</td>
-        <td>{classDetails.facility_ID}</td>
-        <td>{classDetails.name}</td>
-        <td>{`${classDetails.enrl_CAP} ${classDetails.enrl_TOT}`}</td>
-        <td>{classDetails.ssr_COMPONENT}</td>
+        <td>{`${classDetails.subject} ${classDetails.catalogNo} ${classDetails.classSection}`}</td>
+        <td>{classDetails.courseId}</td>
+        <td>{classDetails.sessionCode}</td>
+        <td>{classDetails.description}</td>
+        <td>{classDetails.instructionMode}</td>
+        <td>{classDetails.csuUnits}</td>
+        <td>{days}</td>
+        <td>{classDetails.facilityId}</td>
+        <td>{classDetails.instructorName}</td>
+        <td>{`${classDetails.enrolledCapacity} ${classDetails.enrolledTotal}`}</td>
+        <td>{classDetails.ssrComponent}</td>
         <td>{classDetails.campus}</td>
-        <td>{classDetails.flat_AMT}</td>
-        <td>View Textbook</td>
+        <td>{classDetails.amount}</td>
+        <td>{classDetails.textbook}</td>
       </tr>
     );
   }
