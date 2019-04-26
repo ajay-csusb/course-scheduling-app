474006d343231fb5408863fe22bb58e28c12645a
Ajay Kotian <ajay.kotian@csusb.edu>
2019-04-26 09:49:36 -0700

    fix: don't filter classes if course number is empty

--- src/public/js/CourseNumber.tsx
+++ src/public/js/CourseNumber.tsx
@@ -2,9 +2,12 @@ import { UserInput } from './UserInput';
 import { IClass } from './Class';
 export class CourseNumber {
 
-  public static filter(classes: IClass[] , uInput: UserInput): IClass[] {
+  public static filter(classes: IClass[], uInput: UserInput): IClass[] {
     const result: IClass[] = [];
     const courseNoInstance = new CourseNumber();
+    if (uInput.getCourseNo().length === 0) {
+      return classes;
+    }
     classes.forEach((_class: IClass) => {
       if (courseNoInstance.doesCourseIdMatchUserInput(_class, uInput)) {
         result.push(_class);
