e6cb1ed6c6163402f904ad2225234b71a699bcaa
Ajay Kotian <ajay.kotian@csusb.edu>
2019-06-04 15:52:47 -0700

    chore: remove redundant code

--- src/public/js/Instructor.tsx
+++ src/public/js/Instructor.tsx
@@ -1,13 +1,6 @@
-import * as ClassSearchUtils from './ClassSearchUtils';
 import { UserInput } from './UserInput';
 import { IClass } from './Class';
 export class Instructor {
-
-  static instructorUrl = 'https://webdx.csusb.edu/ClassSchedule/getDropDownList ';
-  public static getAllInstructors(onSuccess: (response: any) => void,
-                                  onFailure: (error: string) => void, url: string = this.instructorUrl): void {
-    ClassSearchUtils.fetchData(url, onSuccess, onFailure);
-  }
 
   public static filter(classes: IClass[] , uInput: UserInput): IClass[] {
     const result: IClass[] = [];
