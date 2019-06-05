8a6d2699655467aff1afb474435959152ccbbfb9
Ajay Kotian <ajay.kotian@csusb.edu>
2019-06-05 16:29:37 -0700

    refactor: add additional parameters to filter method

--- src/public/js/MeetingTime.tsx
+++ src/public/js/MeetingTime.tsx
@@ -1,15 +1,12 @@
-import { UserInput } from './UserInput';
 import { IClass } from './Class';
 import * as ClassSearchUtils from './ClassSearchUtils';
 export class MeetingTime {
 
-  public static filter(classes: IClass[], uInput: UserInput): IClass[] {
+  public static filter(classes: IClass[], startTime: Date, endTime: Date): IClass[] {
     const result: IClass[] = [];
     classes.forEach((_class: IClass) => {
-      const userInputStartTime: Date = uInput.getStartTime();
-      const userInputEndTime: Date = uInput.getEndTime();
-      const userInputStartTimeParsed = ClassSearchUtils.getDateIn12HourFormat(userInputStartTime);
-      const userInputEndTimeParsed = ClassSearchUtils.getDateIn12HourFormat(userInputEndTime);
+      const userInputStartTimeParsed = ClassSearchUtils.getDateIn12HourFormat(startTime);
+      const userInputEndTimeParsed = ClassSearchUtils.getDateIn12HourFormat(endTime);
       if (!ClassSearchUtils.compareStartTimes(userInputStartTimeParsed, _class.classStartTime)) {
         return;
       }
