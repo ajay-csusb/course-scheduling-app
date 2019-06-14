7ad096e7a8457c2cafbd0fcbeaf0ae3e2b86fc30
Ajay Kotian <ajay.kotian@csusb.edu>
2019-06-13 17:43:10 -0700

    test: refactor method

--- cypress/integration/SessionCodeTests.js
+++ cypress/integration/SessionCodeTests.js
@@ -8,36 +8,20 @@ describe('Filter classes by session code', function () {
       before(function () {
         cy.visit(localUrl);
         cy.get('.search-autocomplete input').type('all').click();
-        cy.get('div').contains('all', { timeout: 15000 }).click();
+        cy.wait(5000);
+        cy.get('span').contains('all', { timeout: 20000 }).click();
         cy.get('#additional-filters').click();
         cy.get('.session-code select').select('10 weeks');
         cy.get('button').contains('Submit').click();
         cy.wait(25000);
       });
-      assert10WeeksClasses();
-    });
-
-    context('when a user searches by subject and followed by 10 weeks as session code', () => {
-      before(function () {
-        cy.visit(localUrl);
-        cy.get('.search-autocomplete input').type('All').click();
-        cy.get('div').contains('all', { timeout: 15000 }).click();
-        cy.get('button').contains('Submit').click();
-        cy.get('#additional-filters').click();
-        cy.get('.session-code select').select('10 weeks');
-        cy.get('button').contains('Submit').click();
-        cy.wait(25000);
+      it('should show classes that are 10 weeks long', () => {
+        cy.get('h5').should('contain', 'THESIS');
+        cy.get('h5').should('contain', 'MBA PORTFOLIO');
+        cy.get('h5').should('contain', 'CONTINUOUS ENROLLMENT');
+        cy.get('h5').should('contain', 'GRAD INDEPENDENT STUDY');
+        cy.get('span').should('contain', '10 weeks');
       });
-      assert10WeeksClasses();
     });
   });
 });
-
-function assert10WeeksClasses() {
-  it('should show classes that are 10 weeks long', () => {
-    cy.get('h5').should('contain', 'THESIS');
-    cy.get('h5').should('contain', 'MBA PORTFOLIO');
-    cy.get('h5').should('contain', 'CONTINUOUS ENROLLMENT');
-    cy.get('h5').should('contain', 'GRAD INDEPENDENT STUDY');
-  });
-}
