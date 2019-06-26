cc385707cfa7115f6e66ae4863a9a1a71f91c90d
Ajay Kotian <ajay.kotian@csusb.edu>
2019-06-26 15:08:50 -0700

    test: meeting time component

--- test/ControlGroupMeetingTime.test.tsx
+++ test/ControlGroupMeetingTime.test.tsx
@@ -5,30 +5,42 @@ import fetchMock from 'fetch-mock';
 // tslint:disable:max-line-length
 describe('test control group meeting time component', () => {
 
-    beforeAll(() => {
-        fetchMock.mock('*', {});
-    });
+  beforeAll(() => {
+    fetchMock.mock('*', {});
+  });
 
-    test('start time by default', () => {
-        const classSearchContainerWrapper = mount(<ClassSearchContainer />);
-        expect(classSearchContainerWrapper.state('startTime')).toEqual(new Date('1899-01-01T00:00:00'));
-    });
+  test('start time by default', () => {
+    const classSearchContainerWrapper = mount(<ClassSearchContainer />);
+    expect(classSearchContainerWrapper.state('startTime')).toEqual(new Date('1899-01-01T00:00:00'));
+  });
 
-    test.only('start time when hour arrow is clicked', () => {
-        const classSearchContainerWrapper = mount(<ClassSearchContainer />);
-        classSearchContainerWrapper.find('.start-time span.bp3-timepicker-hour .bp3-icon-chevron-up').simulate('click', { target: { value: new Date('1899-01-01T08:00:00'} });
-        expect(classSearchContainerWrapper.state('startTime')).toEqual(new Date('1899-01-01T08:00:00'));
-    });
+  test('start time when hour arrow is clicked', () => {
+    const classSearchContainerWrapper = mount(<ClassSearchContainer />);
+    classSearchContainerWrapper.find('.start-time span.bp3-timepicker-hour .bp3-icon-chevron-up').simulate('click');
+    classSearchContainerWrapper.find('button[type="submit"]').simulate('click');
+    expect(classSearchContainerWrapper.state('startTime')).toEqual(new Date('1899-01-01T01:00:00'));
+  });
 
-    test('end time by default', () => {
-        const classSearchContainerWrapper = mount(<ClassSearchContainer />);
-        expect(classSearchContainerWrapper.state('endTime')).toEqual(new Date('1899-01-01T20:00:00'));
-    });
+  test('end time by default', () => {
+    const classSearchContainerWrapper = mount(<ClassSearchContainer />);
+    expect(classSearchContainerWrapper.state('endTime')).toEqual(new Date('1899-01-01T23:00:00'));
+  });
 
-    test('end time when hour arrow is clicked', () => {
-        const classSearchContainerWrapper = mount(<ClassSearchContainer />);
-        classSearchContainerWrapper.find('.end-time span.bp3-timepicker-hour .bp3-icon-chevron-down').simulate('click', { target: { value: new Date('1899-01-01T08:00:00' } });
-        expect(classSearchContainerWrapper.state('endTime')).toEqual(new Date('1899-01-01T19:00:00'));
-    });
+  test('end time when hour arrow is clicked', () => {
+    const classSearchContainerWrapper = mount(<ClassSearchContainer />);
+    classSearchContainerWrapper.find('.end-time span.bp3-timepicker-hour .bp3-icon-chevron-down').simulate('click');
+    classSearchContainerWrapper.find('button[type="submit"]').simulate('click');
+    expect(classSearchContainerWrapper.state('endTime')).toEqual(new Date('1899-01-01T22:00:00'));
+  });
+
+  test('start time and end time when reset is clicked', () => {
+    const classSearchContainerWrapper = mount(<ClassSearchContainer />);
+    classSearchContainerWrapper.find('.start-time span.bp3-timepicker-hour .bp3-icon-chevron-up').simulate('click');
+    classSearchContainerWrapper.find('.end-time span.bp3-timepicker-hour .bp3-icon-chevron-down').simulate('click');
+    classSearchContainerWrapper.find('button[type="submit"]').simulate('click');
+    classSearchContainerWrapper.find('button[type="reset"]').simulate('click');
+    expect(classSearchContainerWrapper.state('startTime')).toEqual(new Date('1899-01-01T00:00:00'));
+    expect(classSearchContainerWrapper.state('endTime')).toEqual(new Date('1899-01-01T23:00:00'));
+  });
 
 });
