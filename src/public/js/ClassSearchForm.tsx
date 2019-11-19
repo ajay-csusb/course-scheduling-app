2c951a287ddb3811cc710f35ba1aa59ab9bb7dea
Ajay Kotian <ajay.kotian@csusb.edu>
2019-11-19 14:03:34 -0800

    style: update CSS class on buttons

--- src/public/js/ClassSearchForm.tsx
+++ src/public/js/ClassSearchForm.tsx
@@ -93,8 +93,8 @@ export class ClassSearchForm extends React.Component<IClassSearchFormProps, {}> 
           onChangeOfInstructionMode={this.props.onChangeOfInstructionMode}
         />
         {advancedFilter}
-        <Button text="Submit" onClick={this.props.onSubmit} type="submit" className="bp3-intent-primary" />
-        <Button text="Reset" onClick={this.props.onReset} style={{ float: 'right' }} type="reset" />
+        <Button text="Submit" onClick={this.props.onSubmit} type="submit" className="btn btn-primary btn-solid" />
+        <Button text="Reset" onClick={this.props.onReset} style={{ float: 'right' }} type="reset" className="btn btn-secondary btn-solid"/>
       </FormGroup >
     );
   }
