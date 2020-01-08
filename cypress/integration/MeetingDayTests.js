98b2c12692606ece7c1a3cd52343acf839e6fe49
Ajay Kotian <ajay.kotian@csusb.edu>
2020-01-08 15:05:49 -0800

    test: verify classes can be searched by instructor name, class number, etc

--- cypress/integration/MeetingDayTests.js
+++ cypress/integration/MeetingDayTests.js
@@ -1,17 +1,16 @@
-const devUrl = 'https://dev-main.csusb.edu/course-schedule';
-const localUrl = 'http://localhost:3000/';
+const url = require('./Utils');
 
 describe('Filter classes by meeting day', function () {
 
   before(function () {
-    cy.visit(localUrl);
+    cy.visit(url);
     cy.get('.search-autocomplete input').type('Biology').click();
     cy.get('div').contains('Biology', { timeout: 15000 }).click();
   });
 
   context('when a user does not check any days', () => {
     it('should should classes all in-person and online classes', () => {
-      cy.get('button').contains('Submit').click();
+      cy.get('.btn-primary').click();
       cy.wait(10000);
       cy.get('span').should('contain', 'M-W');
       cy.get('span').should('contain', 'Tu-Th');
@@ -22,7 +21,7 @@ describe('Filter classes by meeting day', function () {
   context('when a user searches for classes on Monday', () => {
     it('should should classes on Monday', () => {
       cy.get('.mon > .bp3-control-indicator').click();
-      cy.get('button').contains('Submit').click();
+      cy.get('.btn-primary').click();
       cy.wait(10000);
       cy.get('span').should('contain', 'M-W');
       cy.get('span').should('not.contain', 'Tu-Th');
@@ -32,11 +31,24 @@ describe('Filter classes by meeting day', function () {
   context('when a user searches for classes on Monday and Tuesday', () => {
     it('should should classes on Monday and Tuesday', () => {
       cy.get('.tue > .bp3-control-indicator').click();
-      cy.get('button').contains('Submit').click();
+      cy.get('.btn-primary').click();
       cy.wait(50000);
       cy.get('span').should('contain', 'M-W');
       cy.get('span').should('contain', 'Tu-Th');
     });
   });
+});
 
+describe('Given class search form', () => {
+  context('when a user searches only by meeting day', () => {
+    before(function () {
+      cy.visit(url);
+      cy.get('.sat > .bp3-control-indicator').click();
+      cy.get('.btn-primary').click();
+      cy.wait(100);
+    });
+    it('should return more than 1 classes in the results', () => {
+      cy.get('.class-search-results-component p').should('not.contain', 'Found 0 classes');
+    });
+  });
 });
