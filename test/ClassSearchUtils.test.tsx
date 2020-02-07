3db160a3cfb1f4a493b4c648f2534bed3e5860a4
Ajay Kotian <ajay.kotian@csusb.edu>
2020-02-07 14:26:31 -0800

    test: verify multiple class attributes are merged

--- test/ClassSearchUtils.test.tsx
+++ test/ClassSearchUtils.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import fetchMock from 'fetch-mock';
 import * as ClassSearchUtils from '../src/public/js/ClassSearchUtils';
-import { classJson, rawClassesJson} from './ClassesJson';
+import { classJson, rawClassesJson, baseClassJson} from './ClassesJson';
 import { ClassSearchContainer } from '../src/public/js/ClassSearchContainer';
 import { mount, shallow } from 'enzyme';
 import { TestUtils } from './TestUtils';
@@ -680,6 +680,29 @@ describe('Number of available seat', () => {
       twoAvailableSeats.enrolledCapacity = 30;
       twoAvailableSeats.enrolledTotal = 28;
       expect(ClassSearchUtils.getNoOfAvailableSeats(twoAvailableSeats)).toEqual(2);
+    });
+  });
+});
+
+describe('when a user performs a search', () => {
+  describe('and two classes with different class numbers are displayed ', () => {
+    it('should display two classes', () => {
+      const result = ClassSearchUtils.mergeAttributes([classJson, baseClassJson]);
+      expect(result).toHaveLength(2);
+    });
+  });
+
+  describe('and two classes with same class number is displayed', () => {
+    const copyOfClassJson = JSON.parse(JSON.stringify(classJson));
+    it('should display one class', () => {
+      const classes = ClassSearchUtils.mergeAttributes([classJson, copyOfClassJson]);
+      expect(classes).toHaveLength(1);
+    });
+    it('should merge the course attributes', () => {
+      classJson.courseAttr = 'foo';
+      copyOfClassJson.courseAttr = 'bar';
+      const classes = ClassSearchUtils.mergeAttributes([classJson, copyOfClassJson]);
+      expect(classes[0].courseAttr).toEqual('foo, bar');
     });
   });
 });
