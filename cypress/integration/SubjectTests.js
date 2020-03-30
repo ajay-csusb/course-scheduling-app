0ad36493cbe2c36a01412ff978f80375d8f936ba
Ajay Kotian <ajay.kotian@csusb.edu>
2020-03-30 08:27:46 -0700

    test: add more wait time to remove inconsitent results

--- cypress/integration/SubjectTests.js
+++ cypress/integration/SubjectTests.js
@@ -5,7 +5,8 @@ describe('Filter classes by subject', function () {
     it('should show Biology classes', () => {
       cy.visit(form.url);
       form.selectSubject();
-      form.submit(); 
+      cy.get('.btn-primary').click();
+      cy.wait(5000);
       cy.get('#class-search-results-component').should('contain', 'classes found');
       cy.get('#class-search-results-component').should('not.contain', '0 classes found');
     });
