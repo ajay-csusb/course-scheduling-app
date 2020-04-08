2d3e570bb1c9a3bb46626b930e42721d2e97c5d0
Ajay Kotian <ajay.kotian@csusb.edu>
2020-04-08 15:34:55 -0700

    test: verify Fall 2020 GE classes attribute dropdown

--- test/SelectListGeClassesAttributes/SelectListGeClassesAttributes.test.tsx
+++ test/SelectListGeClassesAttributes/SelectListGeClassesAttributes.test.tsx
@@ -2,6 +2,8 @@ import { mount } from 'enzyme';
 import * as React from 'react';
 import { ClassSearchContainer } from '../../src/public/js/ClassSearchContainer';
 import { TestUtils } from '../TestUtils';
+import { IOptionProps } from '@blueprintjs/core';
+import { ISelectListGeClassesAttributesProps, SelectListGeClassesAttributes } from '../../src/public/js/SelectListGeClassesAttributes';
 // tslint:disable:max-line-length
 describe('GE classes attributes component', () => {
 
@@ -9,12 +11,48 @@ describe('GE classes attributes component', () => {
     TestUtils.ajax();
   });
 
-  describe('Given a class search form', () => {
+  describe('Given a select option for GE course attributes', () => {
     test('snapshot', () => {
       const classSearchContainerWrapper = mount(<ClassSearchContainer />);
       classSearchContainerWrapper.find('#additional-filters').simulate('click');
       const geClassesAttributes = classSearchContainerWrapper.find('.select-ge-classes-attr');
       expect(geClassesAttributes).toMatchSnapshot();
+    });
+  });
+
+  const geAttrs: IOptionProps[] = [
+    { value: '', label: 'All' },
+    { value: 'ge-foo', label: 'GE-FOO' },
+    { value: 'bar', label: 'BAR' },
+  ];
+
+  describe('and Fall 2020 is selected as a term', () => {
+    it('should display options related to semester term', () => {
+      const geAttrsProps: ISelectListGeClassesAttributesProps = {
+        currentTerm: '2208',
+        geClassesAttributes: geAttrs,
+        geClassesAttribute: 'ge-foo',
+        onChangeOfGeClassesAttribute: jest.fn(),
+      };
+      const selectListGeClassesAttributesWrapper = mount(<SelectListGeClassesAttributes {...geAttrsProps} />);
+      expect(selectListGeClassesAttributesWrapper.html()).toContain('GE-A1 Oral Communication');
+      expect(selectListGeClassesAttributesWrapper.html()).not.toContain('GE-FOO');
+      expect(selectListGeClassesAttributesWrapper.html()).toMatchSnapshot();
+    });
+  });
+
+  describe('and Fall 2018 is selected as a term', () => {
+    it('should display options related to quarter term', () => {
+      const geAttrsProps: ISelectListGeClassesAttributesProps = {
+        currentTerm: '2207',
+        geClassesAttributes: geAttrs,
+        geClassesAttribute: '',
+        onChangeOfGeClassesAttribute: jest.fn(),
+      };
+      const geAttrsComponent = mount(<SelectListGeClassesAttributes {...geAttrsProps} />);
+      expect(geAttrsComponent.html()).toContain('GE-FOO');
+      expect(geAttrsComponent.html()).not.toContain('GE-A1 Oral Communication');
+      expect(geAttrsComponent.html()).toMatchSnapshot();
     });
   });
 });
