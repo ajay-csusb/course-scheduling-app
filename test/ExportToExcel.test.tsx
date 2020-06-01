ef801b65eb00ebc9fb25ed4ab321d0b1b8b99fac
Ajay Kotian <ajay.kotian@csusb.edu>
2020-06-01 12:35:15 -0700

    refactor: update test

--- test/ExportToExcel.test.tsx
+++ test/ExportToExcel.test.tsx
@@ -6,17 +6,19 @@ import fetchMock from 'fetch-mock';
 import { TestUtils } from './TestUtils';
 
 describe('Export to Excel component', () => {
+  let classes;
+
   beforeAll(() => {
     TestUtils.ajax();
+    classes = [classJson, classPDC];
   });
 
   test('snapshot', () => {
-    const exportToExcelComponent = <ExportToExcel classes={[classJson, classPDC]} />
+    const exportToExcelComponent = <ExportToExcel classes={classes} />
     expect(exportToExcelComponent).toMatchSnapshot();
   });
 
   test('fetch parameters', () => {
-    const classes = [classJson, classPDC];
     const exportToExcelComponent = mount(<ExportToExcel classes={classes} />);
     exportToExcelComponent.find('#export-to-excel').simulate('click');
     const fetchOptions = fetchMock.lastOptions();
