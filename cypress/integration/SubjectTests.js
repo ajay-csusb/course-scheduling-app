c7e05ac6efcc0908239b0ae3cda974d0042b0184
Ajay Kotian <ajay.kotian@csusb.edu>
2020-06-02 09:00:55 -0700

    hotfix: verify BIO classes are shown

--- cypress/integration/SubjectTests.js
+++ cypress/integration/SubjectTests.js
@@ -6,9 +6,8 @@ describe('Filter classes by subject', function () {
       cy.visit(form.url);
       form.selectSubject();
       cy.get('.btn-primary').click();
-      cy.wait(5000);
       cy.get('#class-search-results-component').should('contain', 'classes found');
-      cy.get('#class-search-results-component').should('not.contain', '0 classes found');
+      cy.get('.course').should('have.length.be.gt', 0);
     });
   });
 
