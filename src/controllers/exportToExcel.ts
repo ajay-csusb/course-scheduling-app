e5aa8997c6d9bf4b24b9d64b417ee5a6488d7da4
Ajay Kotian <ajay.kotian@csusb.edu>
2020-06-02 11:52:32 -0700

    fix: send 400 HTTP status code if request is malformed

--- src/controllers/exportToExcel.ts
+++ src/controllers/exportToExcel.ts
@@ -3,7 +3,7 @@ import { getExcelDocument } from '../lib/ExportToExcel';
 
 export function index(req: Request, res: Response): any {
   if (req.body.length === undefined) {
-    return res.send(200);
+    return res.send(400);
   }
   const report = getExcelDocument(req.body);
   return res.send(report);
