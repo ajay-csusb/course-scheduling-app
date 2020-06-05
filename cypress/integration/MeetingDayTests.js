2c6300e93ffa512f188c1d0ef56848c1f1dc0d64
Ajay Kotian <ajay.kotian@csusb.edu>
2020-06-05 16:09:48 -0700

    test: verfiy meeting day filter

--- cypress/integration/MeetingDayTests.js
+++ cypress/integration/MeetingDayTests.js
@@ -1,46 +1,43 @@
-const url = require('./Utils');
+import * as form from './Utils';
 
-describe.skip('Filter classes by meeting day', function () {
-
+describe('when a user searches for classes on Monday and Tuesday', () => {
   before(function () {
-    // @Todo tests that use all Bio results should be consolidated into one.
-    cy.visit(url);
-    cy.get('.search-autocomplete input').type('Biology').click();
-    cy.get('div').contains('Biology', { timeout: 15000 }).click();
+    cy.visit( form.url );
+    form.selectSubject();
+    form.checkDays( [ '.mon', '.tue' ] )
+    form.submit();
   });
 
-  context('when a user does not check any days', () => {
-    it('should should classes all in-person and online classes', () => {
-      cy.get('.btn-primary').click();
-      cy.wait(10000);
-      cy.get('li').should('contain', 'M-W');
-      cy.get('li').should('contain', 'Tu-Th');
-    });
-  });
-
-  // @Todo select Monday and Saturday
-  context('when a user searches for classes on Monday and Tuesday', () => {
-    it('should should classes on Monday and Tuesday', () => {
-      cy.get('.mon > .bp3-control-indicator').click();
-      cy.get('.tue > .bp3-control-indicator').click();
-      cy.get('.btn-primary').click();
-      cy.wait(5000);
-      cy.get('li').should('contain', 'M-W');
-      cy.get('li').should('contain', 'Tu-Th');
-    });
+  it('should should classes on Monday and Tuesday', () => {
+    cy.get( 'li' )
+      .should( 'contain', 'M-W' )
+      .should( 'contain', 'Tu-Th' );
   });
 });
 
-describe('Given class search form', () => {
-  context('when a user searches only by meeting day', () => {
-    before(function () {
-      cy.visit(url);
-      cy.get('.sat > .bp3-control-indicator').click();
-      cy.get('.btn-primary').click();
-      cy.wait(100);
-    });
-    it('should return more than 1 classes in the results', () => {
-      cy.get('.class-search-results-component p').should('not.contain', 'Found 0 classes');
-    });
+describe('when a user does not check any days', () => {
+  before(function () {
+    cy.visit( form.url );
+    form.selectSubject();
+    form.submit();
+  });
+
+  it('should should classes all in-person and online classes', () => {
+    cy.get( 'li' )
+      .should( 'contain', 'M-W' )
+      .should( 'contain', 'Tu-Th' );
+  });
+});
+
+describe('when a user searches only by meeting day', () => {
+  before(function () {
+    cy.visit( form.url );
+    form.checkDays( [ '.sun' ] )
+    form.submit();
+  });
+
+  it('should return more than 1 classes in the results', () => {
+    cy.get( '.class-search-results-component p' )
+      .should( 'not.contain', 'Found 0 classes' );
   });
 });
