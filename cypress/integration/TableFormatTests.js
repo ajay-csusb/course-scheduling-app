57739ac40f84bfa5694c1bf3d5540b89c81b2140
Ajay Kotian <ajay.kotian@csusb.edu>
2020-06-05 15:04:50 -0700

    test: add integration tests for Grid format

---
+++ cypress/integration/TableFormatTests.js
@@ -0,0 +1,28 @@
+import * as form from './Utils';
+
+context( 'table format', () => {
+  before(() => {
+    cy.visit(form.url);
+    form.selectSubject();
+    form.submit();
+    cy.get('#bp3-tab-title_display-format-tabs_table')
+      .click();
+  });
+
+  it('should display classes in table format', () => {
+    cy.get('.bp3-table-column-name')
+      .should('contain', 'Subjects')
+      .should('contain', 'Title')
+      .should('contain', 'Section')
+      .should('contain', 'Class Number')
+      .should('contain', 'Type')
+      .should('contain', 'Unit')
+      .should('contain', 'Instructor')
+      .should('contain', 'Building')
+      .should('contain', 'Day(s)')
+      .should('contain', 'Time')
+      .should('contain', 'Mode')
+      .should('contain', 'Session')
+  });
+
+});
