9ee6ccef24c05e37a2df6fcfb0321c113bd8726b
Ajay Kotian <ajay.kotian@csusb.edu>
2020-07-27 10:08:38 -0700

    test: fix failing Cypress test

--- cypress/integration/CourseNumberTests.js
+++ cypress/integration/CourseNumberTests.js
@@ -1,10 +1,11 @@
-const { url, enterCourseNumber, submit } = require('./Utils');
+const { url, enterCourseNumber, submit, selectSubject } = require('./Utils');
 
 describe('Filter classes by course number', function () {
   context('given class search form', () => {
     context('when a user searches only by course number', () => {
       before(() => {
         cy.visit(url);
+        selectSubject();
         enterCourseNumber('1000');
         submit();
       });
