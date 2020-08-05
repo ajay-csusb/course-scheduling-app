946358dcaee59930ffc1579cd10fd0955034374b
Ajay Kotian <ajay.kotian@csusb.edu>
2020-08-05 14:16:47 -0700

    test: verify bookstore link has the updated URL

--- test/Textbook.test.tsx
+++ test/Textbook.test.tsx
@@ -15,7 +15,15 @@ describe('Textbook', () => {
   });
 
   test('link', () => {
-    expect(textbook.link()).toEqual('https://www.bkstr.com/csusanbernardinostore/follett-discover-view/booklook?shopBy=discoverViewCourse&bookstoreId=1100&termId=111&divisionDisplayName=&departmentDisplayName=foo&courseDisplayName=999&sectionDisplayName=04');
+    expect(textbook.link()).toEqual(
+      'https://www.bkstr.com/webApp/discoverView?bookstore_id-1=1100&term_id-1=111&div-1=&dept-1=foo&course-1=999&section-1=04'
+    );
   });
 
+  test('Palm desert bookstore link', async () => {
+    textbook.campus = 'PALM';
+    expect(textbook.link()).toEqual(
+      'https://www.bkstr.com/webApp/discoverView?bookstore_id-1=1101&term_id-1=111&div-1=&dept-1=foo&course-1=999&section-1=04'
+    );
+  });
 });
