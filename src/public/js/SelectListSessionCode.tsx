63aef05884163bb01cb991ba3b39d483e81a5fcb
Ajay Kotian <ajay.kotian@csusb.edu>
2021-02-03 11:48:07 -0800

    refactor: remove not needed options for Session code

--- src/public/js/SelectListSessionCode.tsx
+++ src/public/js/SelectListSessionCode.tsx
@@ -8,12 +8,9 @@ export interface ISelectListSessionCodeProps {
 
 export class SelectListSessionCode extends React.Component<ISelectListSessionCodeProps, {}> {
   private readonly sessionCodes: IOptionProps[] = [
-    { label: 'All', value: 'all' },
-    { label: 'Fall and Spring (Regular)', value: '1' },
     { label: 'Summer terms (6 Week 1)', value: '6W1' },
     { label: 'Summer terms (6 Week 2)', value: '6W2' },
     { label: 'Summer terms (10 Week)', value: '10W' },
-    { label: 'Winter Intersession (3 Week 1)', value: '3W1' },
   ];
 
   public render(): React.ReactNode {
