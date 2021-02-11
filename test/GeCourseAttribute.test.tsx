877b47eca18099eb2a36f48e0a1e491c5925b866
Ajay Kotian <ajay.kotian@csusb.edu>
2021-02-11 14:33:55 -0800

    test: verify classes having GE-C1 and GE-C4 attributes are filtered correctly

--- test/GeCourseAttribute.test.tsx
+++ test/GeCourseAttribute.test.tsx
@@ -5,40 +5,47 @@ import { GeCourseAttribute } from '../src/public/js/GeCourseAttribute';
 
 describe('Filter by GE course attribute', () => {
   const classBioGe = TestUtils.copyObject(classJson);
-  const classBioGe1 = TestUtils.copyObject(classJson);
-  let classesBio: IClass[];
+  const classArtsGe1 = TestUtils.copyObject(classJson);
+  const classArtsGe2 = TestUtils.copyObject(classJson);
+  let classesGe: IClass[];
   let classes: IClass[];
 
   beforeEach(() => {
     classBioGe.courseAttr = 'GE-F0 Life Sciences';
     classBioGe.courseAttrDescription = 'Life Science';
     classBioGe.geCourseAttr = 'GE-F0, DES';
-    classBioGe1.courseAttr = 'General Education, GE-B5 UD Scientific Inquiry & Quant.';
-    classBioGe1.courseAttrDescription = 'UD Scientific Inquiry & Quant.';
-    classBioGe1.geCourseAttr = 'GE-B5, DES';
-    classesBio = [classBioGe, classBioGe1];
+    classArtsGe1.courseAttr =
+      'Diversity & Inclusiveness Pers, Global Perspectives, Writing Intensive, GE-C4 UD Arts and Humanities';
+    classArtsGe1.courseAttrDescription =
+      'Diversity & Inclusiveness Pers, Global Perspectives, Student Opinion - Paper, UD Arts and Humanities, Upper Division, Writing Intensive';
+    classArtsGe1.geCourseAttr = 'GE-C4, DI, G, Y';
+    classArtsGe2.courseAttr = '"Diversity & Inclusiveness Pers, GE-C1 Arts';
+    classArtsGe2.courseAttrDescription = 'Arts, Diversity & Inclusiveness Pers, Lower Divison, Student Opinion - Paper';
+    classArtsGe2.geCourseAttr = 'GE-C1, DI, 1, Y';
+    classesGe = [classBioGe, classArtsGe1, classArtsGe2];
   });
 
   describe('when a user searches for a GE classes', () => {
     describe('if the term is before Fall 2020', () => {
       it('should return all the classes', () => {
         // Course attribute search term should have a length of 5
-        classes = GeCourseAttribute.filter(classesBio, 'GE-F0', '2206');
+        classes = GeCourseAttribute.filter(classesGe, 'GE-F0', '2206');
         expect(classes).toHaveLength(1);
         expect(classes[0].courseAttr).toEqual('GE-F0 Life Sciences');
       });
     });
 
-    describe('and they filter by GE course attribute', () => {
+    describe('if they filter classes by GE course attribute', () => {
       it('should display classes matching the course attribute', () => {
-        classes = GeCourseAttribute.filter(classesBio, 'GE-B5', '2208');
+        classes = GeCourseAttribute.filter(classesGe, 'GE-C1', '2208');
         expect(classes).toHaveLength(1);
-        expect(classes[0].courseAttrDescription).toEqual('GE-B5 UD Scientific Inquiry & Quant.');
+        expect(classes[0].courseAttrDescription).toEqual('GE-C1 Arts');
       });
     });
+
     describe('if they search for invalid GE course attribute', () => {
       it('should not return any classes', () => {
-        classes = GeCourseAttribute.filter(classesBio, 'invalid-option', '2208');
+        classes = GeCourseAttribute.filter(classesGe, 'invalid-option', '2208');
         expect(classes).toHaveLength(0);
       });
     });
